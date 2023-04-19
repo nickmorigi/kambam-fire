b4de9b6ea59e60072a0a6485ce45631630d243e4
Nick Morigi <nicolas.morigi@estudante.uffs.edu.br>
2023-04-19 16:30:13 -0300

    kanban-fire/

---
+++ kanban-fire/src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanban-fire-c61aa',
+    appId: '1:864952853458:web:43a515d986382c42141e0b',
+    storageBucket: 'kanban-fire-c61aa.appspot.com',
+    apiKey: 'AIzaSyBGsMtbaJeiiqzaXuYFZ_CzzCYwgIua9yE',
+    authDomain: 'kanban-fire-c61aa.firebaseapp.com',
+    messagingSenderId: '864952853458',
+    measurementId: 'G-HKN05829BQ',
+  },};
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBGsMtbaJeiiqzaXuYFZ_CzzCYwgIua9yE",
+    authDomain: "kanban-fire-c61aa.firebaseapp.com",
+    projectId: "kanban-fire-c61aa",
+    storageBucket: "kanban-fire-c61aa.appspot.com",
+    messagingSenderId: "864952853458",
+    appId: "1:864952853458:web:43a515d986382c42141e0b",
+    measurementId: "G-HKN05829BQ"
+  };
